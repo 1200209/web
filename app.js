0956e6be0b212a00341764b56e717dacd3396b2c
sasamoto <s1200209.aizu@gmail.com>
2015-05-09 02:17:02 +0900

    test

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+
+var connect = require('connect');
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+//app.listen(8080);
+var http = require('http').createServer(app).listen(8080);
+var io = require('socket.io').listen(http);
+
+
+ //接続
+
+ io.sockets.on('connection', function (socket) {
+     socket.on('count',function(num){ 
+ 	 num.value = Number(num.value) + 1;
+	 console.log(num.value);
+ 	socket.emit('count send',num);
+ 	socket.broadcast.emit('count send',num);
+     });
+
+    
+	
+    
+     // 切断
+     socket.on('disconnect', function () {
+         io.sockets.emit('user disconnected');
+	 console.log('disconnected');
+     });
+ });
+
